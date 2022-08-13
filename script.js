c7e24dbb2b12e99bacd738c81f413fc42aaee2c2
Kohei Matsuno <83019007+Amnis333@users.noreply.github.com>
2022-08-13 15:01:47 +0900

    ログイン画面作成（ログイン機能未実装）

--- script.js
+++ script.js
@@ -4,5 +4,17 @@ const config = {
 
 function makingBtns(btn1, btn2){
     let container = document.createElement("div");
-    
+    container.innerHTML = `
+    <div class="d-flex col-12 justify-content-between">
+        <div class="col-6">
+            <button class="btn btn-primary btn-1">${btn1}</button>
+        </div>
+        <div class="col-6 ">
+            <button class="btn btn-primary btn-2">${btn2}</button>
+        </div>
+    </div>
+    `;
+    return container
 }
+
+document.getElementById("loginBox").append(makingBtns("New","Login"));
